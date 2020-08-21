b9adee6b3b46e1b84ebc6fb033cd0f4ba752daba
Muniro <Munir_Syed@hotmail.com>
2020-08-21 18:01:59 +0100

    removes old style, adds new styles, adds animation, adds new CV

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        default: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
+
+      extend: {},
+    },
+    variants: {},
+    plugins: [],
+  },
+};
